69d21bec3c3a2e795773949664b267a8ab3481fc
Ajay Kumar M <ajay.kumar@phases.io>
2024-03-31 13:47:38 +0530

    Admin route added to get an overview of the child process.

--- app.mjs
+++ app.mjs
@@ -1,12 +1,35 @@
 import express from "express";
-import {fibonacci} from "./heavyComputation.mjs";
+import {fibonacci} from "./heavyComputation.mjs";   
+import cluster from "cluster";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
+app.set("view engine","ejs")
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
     console.log(`landing page: ${process.pid}`)
     res.send("Landing page")
 })
+
+app.get("/admin", (req, res) => {
+    const workers = Object.values(cluster.workers);
+    const allWorkerData = workers.map(worker=> {
+        return {
+            workerId: worker.id,
+            pid: worker.process.pid,
+        }
+    })
+
+    return res.render("admin", {allWorkerData: allWorkerData});
+});
 
 // Define routes and middleware
 app.get('/heavy', (req, res) => {
